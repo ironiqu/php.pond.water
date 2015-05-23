e0981342134f8e27d4743a13cb4c5d328d5ca35e
aith <carpe_diem@aith.jp>
2015-05-24 08:33:58 +0900

    edit

--- index.php
+++ index.php
@@ -1,21 +1,44 @@
 <?php
 include_once 'src/ImportHtml.php';
 
+$urlPattern = "/^(\/([\w_\-\.]+\/)?).*$/";
 
-// index.php to top
+// index.php to index
 $url = $_SERVER ["REQUEST_URI"];
+$url = preg_replace ( "/^(\/([\w_\-\.\/]+\/)?).*$/", "$1", $url );
+echo "<br>".$url;
 
-if (preg_match ( "/.*\/index\.php.*/", $url ) || preg_match ( "/^\/([a-z]+)\/$/", $url ) || !array_key_exists ( "content", $_GET )) {
-	header ( "Location: http://" . $_SERVER ["SERVER_NAME"] . preg_replace ( "/^(\/[a-z]+\/).*$/", "$1top/", $url ) );
+if(preg_match ( "/.*\/index\.php.*/", $url )){
+	echo "<br>1".( "Location: http://" . $_SERVER ["SERVER_NAME"] . preg_replace ( $urlPattern, "$1index/", $url ) );
+}
+if(!array_key_exists ( "content", $_GET )){
+	echo "<br>3".( "Location: http://" . $_SERVER ["SERVER_NAME"] . preg_replace ( $urlPattern, "$1index/", $url ) );
+}
+$pageTitle = "";
+$imgPath = $url."data/image/";
+$c = ucfirst ( array_key_exists ( "content", $_GET )?$_GET ["content"] : "index");
+echo "<br>4".$c;
+echo "<br>5".$imgPath;
+echo "<br>6<img src=\"$imgPath/dummy.png\">";
+
+
+
+
+
+
+exit();
+if (preg_match ( "/.*\/index\.php.*/", $url ) || preg_match ( "/^\/(\/[\w_\-\.]+\/)\/$/", $url ) || !array_key_exists ( "content", $_GET )) {
+	header ( "Location: http://" . $_SERVER ["SERVER_NAME"] . preg_replace ( "/^(\/([\w_\-\.]+\/)?).*$/", "$1index/", $url ) );
 	exit ();
 }
-
 // page create
 $pageTitle = "";
 $imgPath = "../data/image/";
-$c = $c = ucfirst ( $_GET ["content"]);
-
-if (($pageTitle = SrcConst::getTextData ( "page" . $c ))=== "null") {
+$c = ucfirst ( $_GET ["content"]);
+echo $c;
+echo "<br>".$url;
+exit();
+if (($pageTitle = SrcConst::getTextData ( "page" . $c )) === "null") {
 	header ( "Location: http://" . $_SERVER ["SERVER_NAME"] . preg_replace ( "/^(.*\/).+\/$/", "$1", $url ) );
 	exit ();
 }
